3749acf21863be581b944c25e4169e7efc34c1ca
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-05-03 22:55:01 +0300

    Fixing...

--- application/products/models.py
+++ application/products/models.py
@@ -5,7 +5,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     count = db.Column(db.Integer, nullable=False)
-    groupId = db.Column(db.Integer, db.ForeignKey('grp.id'), nullable=False)
+    groupid = db.Column(db.Integer, db.ForeignKey('grp.id'), nullable=False)
 
 
     def __init__(self, name, count, groupId):
